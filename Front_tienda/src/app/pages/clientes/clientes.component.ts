9090b2b515f85bf1f80e78d72d48aac0dc42b611
Jose Andres Calle Montes <joancamo2914@gmail.com>
2021-11-23 19:03:46 -0500

    Agregado botón leer
    Se agregó el botón de leer al sprint clientes, el botón busca en la bd con el nombre del cliente. y las muestra en las casillas del formulario html. En el typescript cree otras variables res y contenido para el método leerClientes(). No se  si es necesario hacer eso.

--- Front_tienda/src/app/pages/clientes/clientes.component.ts
+++ Front_tienda/src/app/pages/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes',
@@ -8,54 +9,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  res:any;
-  contenido:any;
-  urlget:string="http://localhost:8080/api/clientes";
+  res: any;
+  contenido: any;
+  urlget: string = "http://localhost:8080/api/clientes";
 
 
 
-  constructor(private objetohttp:HttpClient) {}
+  constructor(private objetohttp: HttpClient) { }
 
   ngOnInit() {
 
-    this.res=this.objetohttp.get(this.urlget)
-    this.res.subscribe((data:any[])=>{
-      this.contenido=data;
+    this.res = this.objetohttp.get(this.urlget)
+    this.res.subscribe((data: any[]) => {
+      this.contenido = data;
       console.log(this.contenido);
     });
 
 
   }
+  cedula!: number;
+  nombrecliente!: string;
+  direccion!: string;
+  telefono!: string;
+  emailcliente!: string;
 
 
-
-  cedula!:number;
-  nombrecliente!:string;
-  direccion!:string;
-  telefono!:string;
-  emailcliente!:string;
-  
-  
-  codigorespuesta!:number;
-  postCliente(){
+  codigorespuesta!: number;
+  postCliente() {
     this.objetohttp.post<any>(
       "http://localhost:8080/api/clientes",
       {
-      "cedula": this.cedula,
-      "nombrecliente": this.nombrecliente,
-      "direccion": this.direccion,
-      "telefono": this.telefono,
-      "emailcliente": this.emailcliente
+        "cedula": this.cedula,
+        "nombrecliente": this.nombrecliente,
+        "direccion": this.direccion,
+        "telefono": this.telefono,
+        "emailcliente": this.emailcliente
       },
-      {observe:'response'}
-    ).subscribe(response=>{
-      this.codigorespuesta=response.status;
+      { observe: 'response' }
+    ).subscribe(response => {
+      this.codigorespuesta = response.status;
     });
   }
 
+  resLeer: Observable<any>;
+  contenidoLeer: any;
+
+  getClientes() {
+
+    let params = new HttpParams().set('nombrecliente', this.nombrecliente);
+    this.objetohttp.get(this.urlget, { params }).
+      subscribe((resLeer) => {
+        this.contenidoLeer = resLeer
+        console.log(this.contenidoLeer)
+        console.log(this.contenidoLeer[0].nombrecliente)
+        console.log(this.contenidoLeer[0].cedula)
+        console.log(this.contenidoLeer[0].direccion)
+        console.log(this.contenidoLeer[0].telefono)
+        console.log(this.contenidoLeer[0].emailcliente)
+
+        this.nombrecliente=this.contenidoLeer[0].nombrecliente
+        this.cedula=this.contenidoLeer[0].cedula
+        this.direccion=this.contenidoLeer[0].direccion
+        this.telefono=this.contenidoLeer[0].telefono
+        this.emailcliente=this.contenidoLeer[0].emailcliente
+      })
+    }
+updateClientes(){
+  
+}
+
   reloadCurrentPage() {
     window.location.reload();
-   }
-
-
+  }
 }
