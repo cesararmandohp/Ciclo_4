07a4c8c6e1b9d9562285c70df379fae37740b1b5
Jose Andres Calle Montes <joancamo2914@gmail.com>
2021-11-28 16:51:58 -0500

    botón delete modificado
    Modifique el botón de delete, ahora puede buscar por cedula, encontrar la id de esa cédula y eliminarla.

--- Front_tienda/src/app/pages/clientes/clientes.component.ts
+++ Front_tienda/src/app/pages/clientes/clientes.component.ts
@@ -9,13 +9,26 @@ import { Observable } from 'rxjs';
 })
 export class ClientesComponent implements OnInit {
 
-  res: any;
-  contenido: any;
-  urlget: string = "http://localhost:8080/api/clientes";
-
 
 
   constructor(private objetohttp: HttpClient) { }
+
+  res: any;
+  contenido: any;
+  resLeer: Observable<any>;
+  contenidoLeer: any;
+
+  urlget = 'http://localhost:8080/api/clientes';
+  cedula!: number;
+  nombrecliente!: string;
+  direccion!: string;
+  telefono!: string;
+  emailcliente!: string;
+
+  codigorespuesta!: number;
+  codigoget!: number;
+  codigoupdate!: number;
+  codigodelete!: number;
 
   ngOnInit() {
 
@@ -24,42 +37,30 @@ export class ClientesComponent implements OnInit {
       this.contenido = data;
       console.log(this.contenido);
     });
-
-
   }
-  cedula!: number;
-  nombrecliente!: string;
-  direccion!: string;
-  telefono!: string;
-  emailcliente!: string;
 
-
-  codigorespuesta!: number;
   postCliente() {
     this.objetohttp.post<any>(
-      "http://localhost:8080/api/clientes",
+      'http://localhost:8080/api/clientes',
       {
-        "cedula": this.cedula,
-        "nombrecliente": this.nombrecliente,
-        "direccion": this.direccion,
-        "telefono": this.telefono,
-        "emailcliente": this.emailcliente
+        cedula: this.cedula,
+        nombrecliente: this.nombrecliente,
+        direccion: this.direccion,
+        telefono: this.telefono,
+        emailcliente: this.emailcliente
       },
       { observe: 'response' }
     ).subscribe(response => {
       this.codigorespuesta = response.status;
     });
   }
-
-  resLeer: Observable<any>;
-  contenidoLeer: any;
-
   getClientes() {
-
-    //let params = new HttpParams().set('nombrecliente', this.nombrecliente);
-    this.objetohttp.get(this.urlget+"/nombrecliente/"+this.nombrecliente).
+    const urlParaLeer = this.urlget + '/nombrecliente/' + this.nombrecliente;
+    this.objetohttp.get(urlParaLeer).
       subscribe((resLeer) => {
         this.contenidoLeer = resLeer
+
+        console.log(this.contenidoLeer)
         console.log(this.contenidoLeer)
         console.log(this.contenidoLeer[0].nombrecliente)
         console.log(this.contenidoLeer[0].cedula)
@@ -67,45 +68,55 @@ export class ClientesComponent implements OnInit {
         console.log(this.contenidoLeer[0].telefono)
         console.log(this.contenidoLeer[0].emailcliente)
 
-        this.nombrecliente=this.contenidoLeer[0].nombrecliente
-        this.cedula=this.contenidoLeer[0].cedula
-        this.direccion=this.contenidoLeer[0].direccion
-        this.telefono=this.contenidoLeer[0].telefono
-        this.emailcliente=this.contenidoLeer[0].emailcliente
+        this.nombrecliente = this.contenidoLeer[0].nombrecliente
+        this.cedula = this.contenidoLeer[0].cedula
+        this.direccion = this.contenidoLeer[0].direccion
+        this.telefono = this.contenidoLeer[0].telefono
+        this.emailcliente = this.contenidoLeer[0].emailcliente
         console.log(this.urlget)
-        
+
       })
-    }
+  }
 
-updateClientes(){
+  updateClientes() {
 
-  //buscar la id de la cedula
+    // buscar la id de la cedula
 
-  let urlupdate = this.urlget+"/cedula/"+this.cedula;
-  this.res = this.objetohttp.get(urlupdate)
-  this.res.subscribe((data: any)=>
-  {
-    this.contenido=data
-    urlupdate = this.urlget+"/"+this.contenido[0].id;
+    let urlupdate = this.urlget + '/cedula/' + this.cedula;
+    this.res = this.objetohttp.get(urlupdate)
+    this.res.subscribe((data: any) => {
+      this.contenido = data
+      urlupdate = this.urlget + '/' + this.contenido[0].id;
 
-    this.objetohttp.put(urlupdate,
-      {
-        "nombrecliente": this.nombrecliente,
-        "direccion": this.direccion,
-        "telefono": this.telefono,
-        "emailcliente": this.emailcliente
-      },
-    {observe:"response"}
-    ).subscribe(response => {
-      this.codigorespuesta = response.status;
-      console.log(this.codigorespuesta);
+      this.objetohttp.put(urlupdate,
+        {
+          'nombrecliente': this.nombrecliente,
+          direccion: this.direccion,
+          telefono: this.telefono,
+          emailcliente: this.emailcliente
+        },
+        { observe: 'response' }
+      ).subscribe(response => {
+        this.codigoupdate = response.status;
+        console.log(this.codigorespuesta);
+      });
+
     });
+    // usar la id en el metodo htpp put
+  }
 
-  });
-//usar la id en el metodo htpp put
+  deleteCliente() {
+    let urldelete = this.urlget + '/cedula/' + this.cedula;
+    this.res = this.objetohttp.get(urldelete)
+    this.res.subscribe((data: any) => {
+      this.contenido = data
+      urldelete = this.urlget + '/' + this.contenido[0].id;
 
-
-}
+      this.objetohttp.delete(urldelete, { observe: 'response' }).subscribe(response => {
+        this.codigodelete = response.status;
+      });
+    });
+  }
 
   reloadCurrentPage() {
     window.location.reload();
